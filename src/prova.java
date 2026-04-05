a0e4a51f207cbbc3a21e72280824e6da492cbd35
luiizz72 <169167567+luiizz72@users.noreply.github.com>
2026-04-05 15:41:56 -0300

    Estudos

---
+++ src/prova.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+public class prova{
+    static void main(String[] args) {
+       /* 1) Scanner leia = new Scanner(System.in);
+
+                System.out.println("Olá, ");
+        leia.nextLine();
+    }
+}*/
+
+       /* int n1;
+        int n2;
+        Scanner soma = new Scanner(System.in);
+
+        System.out.println("Digite um número");
+        n1 = soma.nextInt();
+        System.out.println("Digite um número");
+        n2 = soma.nextInt();
+
+        int numero = (n1 + n2);
+        System.out.println(""+numero);
+    }}*/
+
+       /* int n1;
+        int n2;
+        Scanner maior = new Scanner(System.in);
+
+        System.out.println("Digite um número ");
+        n1 = maior.nextInt();
+        System.out.println("Digite um número ");
+        n2 = maior.nextInt();
+
+
+        if (n1 > n2){
+            System.out.println(" Maior " +  n1);
+        } else{
+        System.out.println(" Menor " + n2);
+    }}}*/
+
+
+        /*Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero = leitura.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é PAR");
+        } else {
+            System.out.println("O número é ÍMPAR");
+        }
+    }
+}*/
+
+   /*     Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um número pra fazer a tabuada dele: ");
+        int numero = leitura.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}*/
+        /*Scanner media = new Scanner(System.in);
+
+        System.out.println("Digite uma média: ");
+        int media1 = media.nextInt();
+        System.out.println("Digite duas média: ");
+        int media2 = media.nextInt();
+        System.out.println("Digite três média: ");
+        int media3 = media.nextInt();
+
+        int total= ((media1 + media2 + media3 )/3);
+        System.out.println("O resultado das médias são: "+ total);
+
+        if (total >= 7) {
+            System.out.println("Aprovado");
+        } else if (total >= 5 && total < 7) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }}}*/
+
+           /*     Scanner numero = new Scanner(System.in);
+                int contador = 0;
+
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println("Digite um número:");
+                    int n = numero.nextInt();
+
+                    if (n % 2 == 0) {
+                        contador++;
+                    }
+                }
+
+                System.out.println("Quantidade de números pares: " + contador);
+            }
+        }*/
+
+
+    }}
